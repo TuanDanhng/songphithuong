25b82e5d8bfaa875c8eb7e8e6edbd4ef35d31556
Nguyen Tuan Danh <tdtensai112@gmail.com>
2024-09-18 00:44:22 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,24 @@
+use axum::{
+    routing::get_service,
+    Router,
+};
+use tower_http::services::ServeDir;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    // Tạo Router để phục vụ tất cả các file từ thư mục "static"
+    let app = Router::new()
+        .nest_service("/", get_service(ServeDir::new("static")).handle_error(|error| async move {
+            (axum::http::StatusCode::INTERNAL_SERVER_ERROR, format!("Unhandled internal error: {}", error))
+        }));
+        
+    let addr: SocketAddr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    println!("Listening on http://{}", addr);
+
+    // Khởi chạy server
+    axum::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
